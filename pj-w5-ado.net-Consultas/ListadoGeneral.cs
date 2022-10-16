bf16df03a1352a278f93adc4e236d07af542c65f
Maribel Maza <80657076+mysticBel@users.noreply.github.com>
2022-10-16 15:06:20 -0500

    guis creadas frmFacturasxDistritos, frmVendedoresxDistritos, ListadoGeneral, MenuPrincipal

---
+++ pj-w5-ado.net-Consultas/ListadoGeneral.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pj_w5_ado.net_Consultas
+{
+    public partial class ListadoGeneral : Form
+    {
+        public ListadoGeneral()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- pj-w5-ado.net-Consultas/Program.cs
+++ pj-w5-ado.net-Consultas/Program.cs
@@ -16,7 +16,7 @@ namespace pj_w5_ado.net_Consultas
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MenuPrincipal());
         }
     }
 }
